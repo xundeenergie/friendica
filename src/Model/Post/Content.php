1f8e8e4653cf4d4a135eb2758e1257c943a857fa
Hypolite Petovan <hypolite@mrpetovan.com>
2024-07-24 15:16:39 -0400

    Fix typo in SQL query in Model\Post\Content->countBySearch

--- src/Model/Post/Content.php
+++ src/Model/Post/Content.php
@@ -141,7 +141,7 @@ class Content
 		if ($uid != 0) {
 			$condition = ["MATCH (`searchtext`) AGAINST (? IN BOOLEAN MODE) AND (NOT `restricted` OR `uri-id` IN (SELECT `uri-id` FROM `post-user` WHERE `uid` = ?))", $search, $uid];
 		} else {
-			$condition = ["MATCH (`searchtext`) AGAINST (? IN BOOLEAN MODE) AND NOT `restricted", $search];
+			$condition = ["MATCH (`searchtext`) AGAINST (? IN BOOLEAN MODE) AND NOT `restricted`", $search];
 		}
 		return DBA::count(SearchIndex::getSearchTable(), $condition);
 	}
