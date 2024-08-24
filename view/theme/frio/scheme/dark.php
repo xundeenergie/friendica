506983aff1fd85311344fa140f155ce466b3c240
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2024-08-24 12:20:23 +0200

    REUSE more work on the themes

--- view/theme/frio/scheme/dark.php
+++ view/theme/frio/scheme/dark.php
@@ -1,5 +1,10 @@
 <?php
 /*
+ * Copyright (C) 2010-2024, the Friendica project
+ * SPDX-FileCopyrightText: 2010-2024 the Friendica project
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ *
  * Name: Dark
  * Licence: AGPL
  * Author: Hypolite Petovan <hypolite@friendica.mrpetovan.com>
