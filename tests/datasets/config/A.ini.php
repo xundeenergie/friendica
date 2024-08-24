c4b58df927393092dc27d8d43fa493f52703634f
Tobias Diekershoff <tobias.diekershoff@gmx.net>
2024-08-24 14:31:41 +0200

    REUSE tests directory

--- tests/datasets/config/A.ini.php
+++ tests/datasets/config/A.ini.php
@@ -1,4 +1,9 @@
 <?php
+
+// SPDX-FileCopyrightText: 2010-2024 the Friendica project
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 /**
  * A test local ini file
  */
--- tests/datasets/config/transformer/C.node.config.php
+++ tests/datasets/config/transformer/C.node.config.php
@@ -1,5 +1,9 @@
 <?php
 
+// SPDX-FileCopyrightText: 2010-2024 the Friendica project
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 return [
 	'config' => [
 		'hostname' => 'friendica.local',
