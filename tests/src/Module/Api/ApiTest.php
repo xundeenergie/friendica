cb6269eddb2051a93c7774a5881416187b3e872f
Art4 <art4@wlabs.de>
2024-12-13 22:47:32 +0000

    Fix path to tests

--- tests/src/Module/Api/ApiTest.php
+++ tests/src/Module/Api/ApiTest.php
@@ -190,7 +190,7 @@ abstract class ApiTest extends FixtureTestCase
 	public function installAuthTest()
 	{
 		$addon           = 'authtest';
-		$addon_file_path = __DIR__ . '/../../../Util/authtest/authtest.php';
+		$addon_file_path = __DIR__ . '/Util/authtest/authtest.php';
 		$t               = @filemtime($addon_file_path);
 
 		@include_once($addon_file_path);
