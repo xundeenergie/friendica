d9af35766325a96726af97a12213cb31fa9c7d0f
Art4 <art4@wlabs.de>
2025-01-10 21:58:21 +0000

    Create LegacyLoggerFactory

---
+++ src/Core/Logger/Factory/LegacyLoggerFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+// Copyright (C) 2010-2024, the Friendica project
+// SPDX-FileCopyrightText: 2010-2024 the Friendica project
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace Friendica\Core\Logger\Factory;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use Psr\Log\NullLogger;
+
+/**
+ * Manager for the core logging instances
+ */
+final class LegacyLoggerFactory implements LoggerFactory
+{
+	/**
+	 * Creates and returns a PSR-3 Logger instance.
+	 *
+	 * Calling this method multiple times with the same parameters SHOULD return the same object.
+	 *
+	 * @param \Psr\Log\LogLevel::* $logLevel The log level
+	 * @param \Friendica\Core\Logger\Capability\LogChannel::* $logChannel The log channel
+	 */
+	public function createLogger(string $logLevel, string $logChannel): LoggerInterface
+	{
+		return new NullLogger();
+	}
+}
