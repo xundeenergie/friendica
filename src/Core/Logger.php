9d09b105375ae57b882a7fbc810d0f893e543e46
Art4 <art4@wlabs.de>
2025-01-12 13:07:05 +0000

    Remove unused Friendica\Core\Logger::enableWorker() and disableWorker()

--- src/Core/Logger.php
+++ src/Core/Logger.php
@@ -19,57 +19,11 @@ use Psr\Log\LoggerInterface;
 class Logger
 {
 	/**
-	 * LoggerInterface The default Logger type
-	 *
-	 * @var string
-	 */
-	const TYPE_LOGGER = LoggerInterface::class;
-	/**
-	 * WorkerLogger A specific worker logger type, which can be enabled
-	 *
-	 * @var string
-	 */
-	const TYPE_WORKER = WorkerLogger::class;
-	/**
-	 * @var string $type LoggerInterface The current logger type
-	 */
-	private static $type = self::TYPE_LOGGER;
-
-	/**
 	 * @return LoggerInterface|WorkerLogger
 	 */
 	private static function getInstance()
 	{
-		if (self::$type === self::TYPE_LOGGER) {
-			return DI::logger();
-		} else {
-			return DI::workerLogger();
-		}
-	}
-
-	/**
-	 * Enable additional logging for worker usage
-	 *
-	 * @deprecated
-	 *
-	 * @param string $functionName The worker function, which got called
-	 *
-	 * @throws \Friendica\Network\HTTPException\InternalServerErrorException
-	 */
-	public static function enableWorker(string $functionName)
-	{
-		self::$type = self::TYPE_WORKER;
-		DI::workerLogger()->setFunctionName($functionName);
-	}
-
-	/**
-	 * Disable additional logging for worker usage
-	 *
-	 * @deprecated
-	 */
-	public static function disableWorker()
-	{
-		self::$type = self::TYPE_LOGGER;
+		return DI::logger();
 	}
 
 	/**
